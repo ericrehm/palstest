f6621e653314ef06bd7efbd7a2a6c1b53c0f3e3e
Eric Rehm <erehm@seabird.com>
2026-09-01 18:59:16 +1000

    modified test

--- testPMTCal3.py
+++ testPMTCal3.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Define PMT and digitized responsivity and gain files
 PMT = 'R9880U20'
-root_dir = '/Users/erehm/src/pals'
+root_dir = '/Users/erehm/src/palstest'
 rel_pmt_dir = 'pmt'
 resp_csv = 'R9880U20_responsivity_digitized.csv'
 gain_csv = 'R9880U20_gain_digitized.csv'
@@ -14,13 +14,13 @@ pmt_cal = PMTCal.from_files(root_dir, rel_pmt_dir, resp_csv, gain_csv, resistanc
 
 # Test PMTcal
 print(f"PMTCal parameters using {PMT}:")
-control_voltage_V = 3.0 
-supply_multiplier = 250
+control_voltage_V = 1 
+supply_multiplier = 475
 wavelength = 532
 print(control_voltage_V, supply_multiplier, wavelength)
 
 print(pmt_cal.resp_model(wavelength))
-print(pmt_cal.gain_model(control_voltage_V * supply_multiplier))
+print(pmt_cal.gain_model(control_voltage_V * supply_multiplier, extrapolate=True))
 
 # Convert a measured voltage ch532_V to optical power using PMTCal
 ch532_V = -1
